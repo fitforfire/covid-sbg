ca38eb64bed6a76c94ac162c8ee15be2ffe70126
Stefan Hofer <stefan.hofer@me.com>
2020-11-08 22:38:10 +0100

    Mobile view

--- docs/js/chart.js
+++ docs/js/chart.js
@@ -1,5 +1,23 @@
 const colors = ['#e6194b', '#3cb44b', '#ffe119', '#4363d8', '#f58231', '#911eb4', '#46f0f0', '#f032e6', '#bcf60c', '#fabebe', '#008080', '#e6beff', '#9a6324', '#fffac8', '#800000', '#aaffc3', '#808000', '#ffd8b1', '#000075', '#808080', '#ffffff', '#000000'];
 
+const typeColors = {
+    'gesamt': '#ffe119',
+    'aktiv': '#e6194b',
+    'genesen': '#3cb44b',
+    'verstorben': '#000000'
+}
+function isMobile() {
+    return window.innerWidth < 980;
+}
+
+let chart;
+
+function destroy() {
+    if (chart) {
+        chart.destroy();
+    }
+}
+
 function buildDistrictChart(gesamt, district, type) {
     const datasets = [];
     Object.keys(gesamt[district]).forEach((town, i) => {
@@ -22,16 +40,18 @@ function buildDistrictChart(gesamt, district, type) {
         datasets[0].hidden = true;
     }
     const ctx = document.getElementById(district);
-    const chart = new Chart(ctx, {
+    destroy();
+    chart = new Chart(ctx, {
         type: 'line',
         data: {
-            labels: datasets[0].data.map(d => d.x),
+            labels: datasets[0].data.map(d => d.x.split("T")[0]),
             datasets
         },
         options: {
             legend: {
-                position: 'left'
-            }
+                position: isMobile() ? 'bottom' : 'left'
+            },
+            aspectRatio: isMobile() ? 0.5 : 2
         }
     });
 }
@@ -50,23 +70,25 @@ function buildTownChart(gesamt, id) {
         datasets.push({
             label: type,
             data: data,
-            borderColor: colors[i % colors.length],
+            borderColor: typeColors[type],
             backgroundColor: 'transparent',
             hidden: false,
         })
     });
     datasets[0].hidden = true;
     const ctx = document.getElementById(id);
-    const chart = new Chart(ctx, {
+    destroy();
+    chart = new Chart(ctx, {
         type: 'line',
         data: {
-            labels: datasets[0].data.map(d => d.x),
+            labels: datasets[0].data.map(d => d.x.split("T")[0]),
             datasets
         },
         options: {
             legend: {
-                position: 'left'
-            }
+                position: isMobile() ? 'bottom' : 'left'
+            },
+            aspectRatio: isMobile() ? 0.5 : 2
         }
     });
 }
